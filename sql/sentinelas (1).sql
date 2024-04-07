1df11e02bc454f2a8379f21c5b80b3f7edf17668
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-04-07 14:51:19 -0300

    SQL

--- sql/sentinelas (1).sql
+++ sql/sentinelas (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 05-Abr-2024 às 15:21
+-- Tempo de geração: 07-Abr-2024 às 17:50
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -24,99 +24,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `coordenador`
---
-
-DROP TABLE IF EXISTS `coordenador`;
-CREATE TABLE IF NOT EXISTS `coordenador` (
-  `id_coor` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `senha` char(16) NOT NULL,
-  `CPF` char(11) NOT NULL,
-  `nascimento` date NOT NULL,
-  `imagem` varchar(255) NOT NULL,
-  `idade` varchar(2) NOT NULL,
-  `funcao` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `RG` char(10) NOT NULL,
-  `telefone` char(11) NOT NULL,
-  `data_entrada` date NOT NULL,
-  `endereco` varchar(255) NOT NULL,
-  `genero` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_coor`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Extraindo dados da tabela `coordenador`
---
-
-INSERT INTO `coordenador` (`id_coor`, `nome`, `email`, `senha`, `CPF`, `nascimento`, `imagem`, `idade`, `funcao`, `RG`, `telefone`, `data_entrada`, `endereco`, `genero`) VALUES
-(4, 'Jean de Souza', 'Jean@gmail.com', '123', '12345678900', '2014-04-01', '', '50', 'uruguaiana', '0000000000', '55999999999', '2014-04-30', 'cohab 2', 'M');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `pais`
---
-
-DROP TABLE IF EXISTS `pais`;
-CREATE TABLE IF NOT EXISTS `pais` (
-  `id_pais` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) NOT NULL,
-  `nom_dan` varchar(255) NOT NULL,
-  `senha` varchar(16) NOT NULL,
-  `CPF` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `idade` varchar(2) NOT NULL,
-  `nacionalidade` varchar(255) NOT NULL,
-  `funcao` varchar(255) NOT NULL,
-  `telefone` varchar(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `imagem` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_pais`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Extraindo dados da tabela `pais`
---
-
-INSERT INTO `pais` (`id_pais`, `nome`, `nom_dan`, `senha`, `CPF`, `idade`, `nacionalidade`, `funcao`, `telefone`, `email`, `imagem`) VALUES
-(3, 'Raqual Mattes Monegelo', 'Luce Terezinha Mattes Mongelo', '123', '80610420020', '48', 'Uruguaiana', 'Mãe', '55999982163', 'Raquelmattes88@gmail.com', '');
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `usuario`
 --
 
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
   `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `status` int DEFAULT NULL,
   `nome` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `datas` date DEFAULT NULL,
-  `CPF` varchar(255) DEFAULT NULL,
-  `RG` varchar(255) DEFAULT NULL,
+  `CPF` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `RG` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `categoria` varchar(255) NOT NULL,
   `senha` varchar(255) DEFAULT NULL,
-  `tipo` varchar(255) DEFAULT NULL,
-  `telefone` varchar(255) DEFAULT NULL,
+  `telefone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `usuario` varchar(255) DEFAULT NULL,
   `imagem` varchar(255) NOT NULL,
   `genero` varchar(255) NOT NULL,
   `endereco` varchar(255) NOT NULL,
   `responsavel` varchar(255) NOT NULL,
   `data_entrada` date NOT NULL,
-  `tele_respon` varchar(255) NOT NULL,
+  `tele_respon` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `idade` varchar(2) NOT NULL,
+  `funcao` varchar(15) NOT NULL,
+  `nom_dan` varchar(255) NOT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `nome`, `email`, `datas`, `CPF`, `RG`, `senha`, `tipo`, `telefone`, `usuario`, `imagem`, `genero`, `endereco`, `responsavel`, `data_entrada`, `tele_respon`, `idade`) VALUES
-(17, 'Antonio Carlos Mattes Mongelo', 'antoniomattes72@gmail.com', '2006-08-10', '05500840029', '2108268794', '123', 'adulto', '5596860344', '2022324018', 'antonionMong.png', 'M', 'cohab 2', 'Raquel Mattes Mongelo', '2022-10-06', '55999982163', '17'),
-(18, 'Luce Terezinha Mattes Mongelo', 'lucemattes2@gmail.com', '2009-12-21', '05500866095', '1234567890', '123', 'adulto ', '5599302422', '2022324010', 'luce.png', 'F', 'cohab 2', 'Raqual Mattes Monegelo ', '2022-10-05', '55999982163', '14');
+INSERT INTO `usuario` (`id_usuario`, `status`, `nome`, `email`, `datas`, `CPF`, `RG`, `categoria`, `senha`, `telefone`, `usuario`, `imagem`, `genero`, `endereco`, `responsavel`, `data_entrada`, `tele_respon`, `idade`, `funcao`, `nom_dan`) VALUES
+(17, 1, 'Antonio Carlos Mattes Mongelo', 'antoniomattes72@gmail.com', '2006-08-10', '05500840029', '2108268794', 'adulto', '123', '5596860344', '2022324018', 'antonionMong.png', 'M', 'cohab 2', 'Raquel Mattes Mongelo', '2022-10-06', '55999982163', '17', 'dançarino', ''),
+(18, 1, 'Luce Terezinha Mattes Mongelo', 'lucemattes2@gmail.com', '2009-12-21', '05500866095', '1234567890', 'adulto', '123', '5599302422', '2022324010', 'luce.jpg', 'F', 'cohab 2', 'Raqual Mattes Mongelo ', '2022-10-05', '55999982163', '14', 'dançarino', ''),
+(20, 2, 'Jean de Souza', 'JeandeSouza@gmail.com', '2033-04-21', '12345678900', '00000000000', 'adulto', '123', '55999999999', '2022325054', 'jean.png', 'M', 'cohab 2', 'Proprio', '2034-04-12', '', '50', 'coordenador', ''),
+(21, 3, 'Raqual Mattes Monegelo', 'Raquelmattes88@gmail.com', '1975-09-12', '80610420020', '00000000000', 'adulto', '123', '55999982163', '2022327439', 'raquel.jpg', 'F', 'cohab 2', 'proprio', '2021-10-06', '', '48', 'mae', 'Luce Terezinha Mattes Mongelo');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
