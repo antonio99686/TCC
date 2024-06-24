1883b7ae84d8d883631464a4b304ad922b2d7ef0
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-06-24 11:23:05 -0300

    EMAIL

--- email/index.php
+++ email/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Notificações</title>
-    <link rel="shortcut icon" href="img/gmail.png" /><link rel="shortcut icon" href="img/gmail.png" />
+    <link rel="shortcut icon" href="img/gmail.png" />
     <!-- Inclua o SweetAlert2 -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
